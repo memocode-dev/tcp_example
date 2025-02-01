e69dfbe2ffaeed2af4fcfd20f4b7ffedd7076e02
audgks5551 <audgsk5551@daum.net>
2025-02-01 16:35:30 +0900

    init

--- src/main/java/dev/memocode/tcp2/TestConfig.java
+++ src/main/java/dev/memocode/tcp2/TestConfig.java
@@ -57,7 +57,7 @@ public class TestConfig {
         TCPMasterConnection connection = null; // Modbus TCP connection
         ModbusTCPTransaction transaction = null; // Modbus TCP transaction
         int unitId = 1; // Modbus Slave Unit ID (1 by default)
-        String modbusHost = "172.30.1.100"; // Modbus server IP
+        String modbusHost = "192.168.50.9"; // Modbus server IP
         int port = 502; // Modbus TCP port
 
         try {
